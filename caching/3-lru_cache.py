1cfed727287f186b6edd3fa157ac81ff384d72ef
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2025-05-12 10:52:31 +0100

    Update 3-lru_cache.py

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -20,7 +20,6 @@ class LRUCache(BaseCaching):
             del self.cache_data[key]
         self.cache_data[key] = item
     def get(self, key):
-        
         """Get an item in the cache"""
         if key is None or key not in self.cache_data:
             return None
